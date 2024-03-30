08676af3aecd13b8878afdbecc5830cd2bbf7984
Thomas Nibler <dev@tnibler.de>
2024-03-30 12:56:32 +0100

    Add get command to read current state

--- switchbox.c
+++ switchbox.c
@@ -9,7 +9,7 @@ const unsigned short DEFAULT_VENDOR = 0x0001;
 const unsigned short DEFAULT_PRODUCT = 0x0000;
 
 const char usage[] = "Usage:\n\
-%s on|off [vendor:product]\n\
+%s on|off|get [vendor:product]\n\
 vendor and product id in format 12ab:cd45 as output by lsusb\n\
 Default: %04x:%04x\n\n\
 Unless you have udev rules set up, this program must probably run as root.\n";
@@ -24,6 +24,8 @@ int main(int argc, char *argv[]) {
     set = 1;
   } else if (!strcmp("off", argv[1]) || !strcmp("OFF", argv[1])) {
     set = 0;
+  } else if (!strcmp("get", argv[1]) || !strcmp("GET", argv[1])) {
+    set = -1;
   } else {
     goto print_help;
   }
@@ -67,29 +69,52 @@ int main(int argc, char *argv[]) {
   }
 
   unsigned char buf[16] = {0};
-  // CONFIGURE command opcode
-  buf[0] = 0x10;
-  // the rest of the buffer is all 0 i.e., configure all pins as output.
-  res = hid_write(handle, buf, 16);
-  if (res < 0) {
-    fprintf(stderr, "Error sending initial CONFIGURE command to HID device:\n%ls\n", hid_error(handle));
-    return 1;
-  }
 
+  if (set == -1) {
+    // READ_ALL command
+    buf[0] = 0x80;
+    res = hid_write(handle, buf, 16);
+    if (res < 0) {
+      fprintf(stderr, "Error sending READ_ALL command to HID device:\n%ls\n", hid_error(handle));
+      return 1;
+    }
+    res = hid_read(handle, buf, 16);
+    if (res < 0) {
+      fprintf(stderr, "Error reading response from to HID device:\n%ls\n", hid_error(handle));
+      return 1;
+    }
+    // byte 10 of response contains bitmap of IO port values
+    // we care about pin 3
+    int state = buf[10] & 0x8;
+    printf("%s\n", state ? "on" : "off");
+  } else if (set == 0 || set == 1) {
+    // CONFIGURE command opcode
+    buf[0] = 0x10;
+    // the rest of the buffer is all 0 i.e., configure all pins as output.
+    res = hid_write(handle, buf, 16);
+    if (res < 0) {
+      fprintf(stderr,
+              "Error sending initial CONFIGURE command to HID device:\n%ls\n",
+              hid_error(handle));
+      return 1;
+    }
 
-  // SET_CLEAR_OUTPUT command opcode
-  buf[0] = 0x08;
-  if (set) {
-    // set pin 3 high
-    buf[11] = 0x08;
-  } else {
-    // clear all pins
-    buf[12] = 0xff;
-  }
-  res = hid_write(handle, buf, 16);
-  if (res < 0) {
-    fprintf(stderr, "Error sending SET_CLEAR_OUTPUT command to HID device:\n%ls\n", hid_error(handle));
-    return 1;
+    // SET_CLEAR_OUTPUT command opcode
+    buf[0] = 0x08;
+    if (set) {
+      // set pin 3 high
+      buf[11] = 0x08;
+    } else {
+      // clear all pins
+      buf[12] = 0xff;
+    }
+    res = hid_write(handle, buf, 16);
+    if (res < 0) {
+      fprintf(stderr,
+              "Error sending SET_CLEAR_OUTPUT command to HID device:\n%ls\n",
+              hid_error(handle));
+      return 1;
+    }
   }
 
   hid_close(handle);
